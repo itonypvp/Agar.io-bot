b3d6a7855e2a1652499defa213128070fad49804
itonypvp <itonypvp@hotmail.com>
2015-10-21 16:08:59 +0400

    AgarioSa

--- launcher.user.js
+++ launcher.user.js
@@ -18,7 +18,7 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposLauncher
 // @namespace   AposLauncher
-// @include     http://agar.io/*
+// @include     http://http://agar.gr.sa/*
 // @version     4.148
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
